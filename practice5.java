d8823b4fe684bed9ca35f8701bca84c8a3f1632e
Matt2334 <89859938+Matt2334@users.noreply.github.com>
2024-01-02 23:51:08 -0500

    Update and rename pe1q4.java to practice5.java

---
+++ practice5.java
@@ -0,0 +1,17 @@
+/* 
+Prompt:
+
+1. Instance variables for the title (String), author (String), and price (float) of the book.
+2. A three-argument constructor to set each of the instance variables above. If the title or author is null or
+the price is less than 0, throw an IllegalArgumentException stating the argument that is not correct.
+3. Get and Set methods for each variable.
+*/
+
+public class practice5{
+    public static void main(String[] args){
+        Book b= new Book("hello", "world", 13.0f);
+        System.out.println(b.getAuthor());
+        b.setAuthor("no");
+        System.out.println(b.getAuthor());
+    }
+}
