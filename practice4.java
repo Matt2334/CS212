eab60fed5ff7bbac1a33fab225cf61c5cb209456
Matt2334 <89859938+Matt2334@users.noreply.github.com>
2024-01-02 23:50:16 -0500

    Update and rename pe1q3.java to practice4.java

--- practice4.java
+++ practice4.java
@@ -1,5 +1,14 @@
+/* 
+Prompt:
+
+Write a Java program that will read a String using a JOptionPane, count the number of characters that are
+digits, and display that number in another JOptionPane. This may make use of the method in the Character
+wrapper class called isDigit().
+ */
+
+
 import javax.swing.*;
-public class pe1q3{
+public class practice4{
     public static void main(String[] args){
         String input=JOptionPane.showInputDialog(null,"Enter something:");
         int total=0;
@@ -9,8 +18,3 @@ public class pe1q3{
         JOptionPane.showMessageDialog(null,"The total number of digits are: "+total);
     }
 }
-/* 
-Write a Java program that will read a String using a JOptionPane, count the number of characters that are
-digits, and display that number in another JOptionPane. This may make use of the method in the Character
-wrapper class called isDigit().
- */
