84c6821a79de0acf31bc1136d706149e87240210
Matt2334 <89859938+Matt2334@users.noreply.github.com>
2024-01-02 23:49:34 -0500

    Update and rename pe1q2.java to practice3.java

--- practice3.java
+++ practice3.java
@@ -1,4 +1,4 @@
-public class pe1q2{
+public class practice3{
 public static void main(String args[]){
     int arr[][] = {{10, 45}, {2, 42, 67, 5}, {3, 21}};
     System.out.println(getSumOfRow(arr, 1)); //prints the sum of row 1
